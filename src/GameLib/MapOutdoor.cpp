38ffb2e01a0daef717927a74d78c5a1330c83ed7
irawork <iraworkcoder@gmail.com>
2025-09-03 03:53:40 +0300

    __HEIGHT_CACHE_TRACE__ Remove

--- src/GameLib/MapOutdoor.cpp
+++ src/GameLib/MapOutdoor.cpp
@@ -769,14 +769,6 @@ float CMapOutdoor::GetCacheHeight(float fx, float fy)
 
 	DWORD dwKey=0;
 
-#ifdef __HEIGHT_CACHE_TRACE__
-	static DWORD s_dwTotalCount=0;
-	static DWORD s_dwHitCount=0;
-	static DWORD s_dwErrorCount=0;
-
-	s_dwTotalCount++;
-#endif
-
 	std::vector<SHeightCache::SItem>* pkVct_kItem=NULL;
 	if (m_kHeightCache.m_isUpdated && nx<16*30000 && ny<16*30000)
 	{
@@ -792,44 +784,11 @@ float CMapOutdoor::GetCacheHeight(float fx, float fy)
 			SHeightCache::SItem& rkItem=*i;
 			if (rkItem.m_dwKey==dwKey)
 			{
-#ifdef __HEIGHT_CACHE_TRACE__
-				s_dwHitCount++;
-				
-				if (s_dwTotalCount>1000)
-				{
-					DWORD dwHitRate=s_dwHitCount*1000/s_dwTotalCount;
-					static DWORD s_dwMaxHitRate=0;
-					if (s_dwMaxHitRate<dwHitRate)
-					{
-						s_dwMaxHitRate=dwHitRate;
-						printf("HitRate %f\n", s_dwMaxHitRate*0.1f);
-					}
-
-
-				}			
-#endif
 				return rkItem.m_fHeight;
 			}
-		}		
+		}
 	}
-	else
-	{
-#ifdef __HEIGHT_CACHE_TRACE__
-		s_dwErrorCount++;
-		//printf("NoCache (%f, %f)\n", fx/100.0f, fy/100.0f);
-#endif
-	}
-#ifdef __HEIGHT_CACHE_TRACE__	
-	if (s_dwTotalCount>=1000000)
-	{
-		printf("HitRate %f\n", s_dwHitCount*1000/s_dwTotalCount*0.1f);
-		printf("ErrRate %f\n", s_dwErrorCount*1000/s_dwTotalCount*0.1f);
-		s_dwHitCount=0;
-		s_dwTotalCount=0;
-		s_dwErrorCount=0;
-	}
-#endif
-	
+
 	float fTerrainHeight = GetTerrainHeight(fx, fy);
 #ifdef SPHERELIB_STRICT
 	if (MAPOUTDOOR_GET_HEIGHT_TRACE)
@@ -874,9 +833,6 @@ float CMapOutdoor::GetCacheHeight(float fx, float fy)
 	{
 		if (pkVct_kItem->size()>=200)
 		{
-#ifdef __HEIGHT_CACHE_TRACE__
-			printf("ClearCacheHeight[%d]\n", dwKey%SHeightCache::HASH_SIZE);
-#endif
 			pkVct_kItem->clear();
 		}
 		
